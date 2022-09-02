0635ff32da9caabb640e6978f3b1834b7b224d68
Pawan Matade <110924450+pawandematade@users.noreply.github.com>
2022-09-02 13:46:47 +0530

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+CONTAINER_NAME="alice-socket"
+IMAGE_NAME="alice-socket"
+SRC=/home/ubuntu/Alice_Socket
+set +x
+echo "=================="
+echo "|Pulling the code|"
+echo "=================="
+echo ""
+cd $SRC && git pull
+echo ""
+
+echo "======================="
+echo "|Building the codebase|"
+echo "======================="
+echo ""
+docker build -t $IMAGE_NAME .
+echo ""
+echo "==========================="
+echo "|DEPLOYING THE APPLICATION|"
+echo "==========================="
+echo ""
+
+
+docker rm -f $CONTAINER_NAME
+
+docker run -d --restart always --name $CONTAINER_NAME $IMAGE_NAME
+
+
+echo ""
+echo "==============================================="
+echo "| $IMAGE_NAME APPLICATION DEPLOYMENT COMPLETED|"
+echo "==============================================="
+echo ""
